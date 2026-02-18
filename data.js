5e5ed4fd9acfb8a14e9f7312db13ffab9eb5370f
uxuridevmx <uxurimx@gmail.com>
2026-02-18 17:36:04 -0600

    v1

---
+++ data.js
@@ -0,0 +1,35 @@
+const unamData = {
+    "datosPersonales": {
+        "numeroCuenta": "323226389",
+        "primerApellido": "GORDILLO",
+        "segundoApellido": "JACOME",
+        "nombres": "JOSE MARTIN"
+    },
+    "detallesAcademicos": {
+        "nivel": "BACHILLERATO",
+        "planEstudios": "AREA III CIENCIAS SOCIALES",
+        "nombrePlantel": "E.N.P. 9 \"PEDRO DE ALBA\"",
+        "promedio": "8.03",
+        "promedioLetra": "Ocho punto cero tres",
+        "avance": "100%",
+        "avanceLetra": "Cien por ciento",
+        "asignaturasAprobadas": "34",
+        "asignaturasNoRequeridas": "0",
+        "asignaturasTotales": "34",
+        "creditosObligatorios": "340",
+        "creditosOptativos": "24",
+        "creditosTotales": "364"
+    },
+    "detallesDocumento": {
+        "nombreDocumento": "CERTIFICADO INTEGRO DE ESTUDIOS - BACHILLERATO",
+        "claveDocumento": "CIB25193019-001-01-01-01-01",
+        "folioPapelSeguridad": "A729422",
+        "nombreResponsable": "LIC. JORGE HERNANDEZ TREJO",
+        "cargoResponsable": "JEFE(A) DE LA UNIDAD DE ADMINISTRACION ESCOLAR",
+        "lugarFechaEmision": "CIUDAD UNIVERSITARIA, CIUDAD DE MÉXICO A 02 DE DICIEMBRE DE 2025",
+        "pagina": "1 DE 1",
+        "jefeCertificados": "LIC. ARELHI MONTES DE OCA ALVAREZ",
+        "directorCertificacion": "LIC. DIANA GONZALEZ NIETO",
+        "directorGeneral": "MTRA. IVONNE RAMIREZ WENCE"
+    }
+};

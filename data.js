c535469b2c955d9f0db421465e51ccd9ecb18c51
uxuridevmx <uxurimx@gmail.com>
2026-02-18 17:55:26 -0600

    v2 paht and sesions

--- data.js
+++ data.js
@@ -1,35 +1,72 @@
-const unamData = {
-    "datosPersonales": {
-        "numeroCuenta": "323226389",
-        "primerApellido": "GORDILLO",
-        "segundoApellido": "JACOME",
-        "nombres": "JOSE MARTIN"
+const unamEntries = {
+    "CIB25193019": {
+        "datosPersonales": {
+            "numeroCuenta": "323226389",
+            "primerApellido": "GORDILLO",
+            "segundoApellido": "JACOME",
+            "nombres": "JOSE MARTIN"
+        },
+        "detallesAcademicos": {
+            "nivel": "BACHILLERATO",
+            "planEstudios": "AREA III CIENCIAS SOCIALES",
+            "nombrePlantel": "E.N.P. 9 \"PEDRO DE ALBA\"",
+            "promedio": "8.03",
+            "promedioLetra": "Ocho punto cero tres",
+            "avance": "100%",
+            "avanceLetra": "Cien por ciento",
+            "asignaturasAprobadas": "34",
+            "asignaturasNoRequeridas": "0",
+            "asignaturasTotales": "34",
+            "creditosObligatorios": "340",
+            "creditosOptativos": "24",
+            "creditosTotales": "364"
+        },
+        "detallesDocumento": {
+            "nombreDocumento": "CERTIFICADO INTEGRO DE ESTUDIOS - BACHILLERATO",
+            "claveDocumento": "CIB25193019-001-01-01-01-01",
+            "folioPapelSeguridad": "A729422",
+            "nombreResponsable": "LIC. JORGE HERNANDEZ TREJO",
+            "cargoResponsable": "JEFE(A) DE LA UNIDAD DE ADMINISTRACION ESCOLAR",
+            "lugarFechaEmision": "CIUDAD UNIVERSITARIA, CIUDAD DE MÉXICO A 02 DE DICIEMBRE DE 2025",
+            "pagina": "1 DE 1",
+            "jefeCertificados": "LIC. ARELHI MONTES DE OCA ALVAREZ",
+            "directorCertificacion": "LIC. DIANA GONZALEZ NIETO",
+            "directorGeneral": "MTRA. IVONNE RAMIREZ WENCE"
+        }
     },
-    "detallesAcademicos": {
-        "nivel": "BACHILLERATO",
-        "planEstudios": "AREA III CIENCIAS SOCIALES",
-        "nombrePlantel": "E.N.P. 9 \"PEDRO DE ALBA\"",
-        "promedio": "8.03",
-        "promedioLetra": "Ocho punto cero tres",
-        "avance": "100%",
-        "avanceLetra": "Cien por ciento",
-        "asignaturasAprobadas": "34",
-        "asignaturasNoRequeridas": "0",
-        "asignaturasTotales": "34",
-        "creditosObligatorios": "340",
-        "creditosOptativos": "24",
-        "creditosTotales": "364"
-    },
-    "detallesDocumento": {
-        "nombreDocumento": "CERTIFICADO INTEGRO DE ESTUDIOS - BACHILLERATO",
-        "claveDocumento": "CIB25193019-001-01-01-01-01",
-        "folioPapelSeguridad": "A729422",
-        "nombreResponsable": "LIC. JORGE HERNANDEZ TREJO",
-        "cargoResponsable": "JEFE(A) DE LA UNIDAD DE ADMINISTRACION ESCOLAR",
-        "lugarFechaEmision": "CIUDAD UNIVERSITARIA, CIUDAD DE MÉXICO A 02 DE DICIEMBRE DE 2025",
-        "pagina": "1 DE 1",
-        "jefeCertificados": "LIC. ARELHI MONTES DE OCA ALVAREZ",
-        "directorCertificacion": "LIC. DIANA GONZALEZ NIETO",
-        "directorGeneral": "MTRA. IVONNE RAMIREZ WENCE"
+    "TEST12345678": {
+        "datosPersonales": {
+            "numeroCuenta": "123456789",
+            "primerApellido": "GARCIA",
+            "segundoApellido": "LOPEZ",
+            "nombres": "MARIA FERNANDA"
+        },
+        "detallesAcademicos": {
+            "nivel": "LICENCIATURA",
+            "planEstudios": "DERECHO",
+            "nombrePlantel": "FACULTAD DE DERECHO",
+            "promedio": "9.50",
+            "promedioLetra": "Nueve punto cinco cero",
+            "avance": "80%",
+            "avanceLetra": "Ochenta por ciento",
+            "asignaturasAprobadas": "40",
+            "asignaturasNoRequeridas": "0",
+            "asignaturasTotales": "50",
+            "creditosObligatorios": "300",
+            "creditosOptativos": "50",
+            "creditosTotales": "350"
+        },
+        "detallesDocumento": {
+            "nombreDocumento": "HISTORIAL ACADÉMICO",
+            "claveDocumento": "HA2025-001",
+            "folioPapelSeguridad": "B999999",
+            "nombreResponsable": "DRA. ANA ROMERO",
+            "cargoResponsable": "DIRECTORA DE LA FACULTAD",
+            "lugarFechaEmision": "CIUDAD UNIVERSITARIA, CIUDAD DE MÉXICO A 10 DE ENERO DE 2026",
+            "pagina": "1 DE 1",
+            "jefeCertificados": "LIC. PEDRO SOTO",
+            "directorCertificacion": "LIC. LUCIA MENDEZ",
+            "directorGeneral": "MTRA. IVONNE RAMIREZ WENCE"
+        }
     }
 };
